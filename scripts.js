f3f857aef3b7fccc4daf485417cf993b76097b76
KarenMkrtchyan <70455623+KarenMkrtchyan@users.noreply.github.com>
2024-04-09 23:10:43 -0700

    finished

--- scripts.js
+++ scripts.js
@@ -1,94 +1,137 @@
-/**
- * Data Catalog Project Starter Code - SEA Stage 2
- *
- * This file is where you should be doing most of your work. You should
- * also make changes to the HTML and CSS files, but we want you to prioritize
- * demonstrating your understanding of data structures, and you'll do that
- * with the JavaScript code you write in this file.
- * 
- * The comments in this file are only to help you learn how the starter code
- * works. The instructions for the project are in the README. That said, here
- * are the three things you should do first to learn about the starter code:
- * - 1 - Change something small in index.html or style.css, then reload your 
- *    browser and make sure you can see that change. 
- * - 2 - On your browser, right click anywhere on the page and select
- *    "Inspect" to open the browser developer tools. Then, go to the "console"
- *    tab in the new window that opened up. This console is where you will see
- *    JavaScript errors and logs, which is extremely helpful for debugging.
- *    (These instructions assume you're using Chrome, opening developer tools
- *    may be different on other browsers. We suggest using Chrome.)
- * - 3 - Add another string to the titles array a few lines down. Reload your
- *    browser and observe what happens. You should see a fourth "card" appear
- *    with the string you added to the array, but a broken image.
- * 
- */
+function populateDialog(dialog, inputCountry, countryDetail) {
+	dialog.replaceChildren() // clear out dialog
+	const image = document.createElement('img')
+	const name = document.createElement('h2')
+	const button = document.createElement('button')
+	const list = document.createElement('ul')
+	const population = document.createElement('li')
+	const area = document.createElement('li')
+	const gdp = document.createElement('li')
+	list.appendChild(population)
+	list.appendChild(area)
+	list.appendChild(gdp)
+	dialog.appendChild(name)
+	dialog.appendChild(image)
+	dialog.appendChild(list)
+	dialog.appendChild(button)
 
-
-const FRESH_PRINCE_URL = "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL = "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL = "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
-
-// This is an array of strings (TV show titles)
-let titles = [
-    "Fresh Prince of Bel Air",
-    "Curb Your Enthusiasm",
-    "East Los High"
-];
-// Your final submission should have much more data than this, and 
-// you should use more than just an array of strings to store it all.
-
-
-// This function adds cards the page to display the data in the array
-function showCards() {
-    const cardContainer = document.getElementById("card-container");
-    cardContainer.innerHTML = "";
-    const templateCard = document.querySelector(".card");
-    
-    for (let i = 0; i < titles.length; i++) {
-        let title = titles[i];
-
-        // This part of the code doesn't scale very well! After you add your
-        // own data, you'll need to do something totally different here.
-        let imageURL = "";
-        if (i == 0) {
-            imageURL = FRESH_PRINCE_URL;
-        } else if (i == 1) {
-            imageURL = CURB_POSTER_URL;
-        } else if (i == 2) {
-            imageURL = EAST_LOS_HIGH_POSTER_URL;
-        }
-
-        const nextCard = templateCard.cloneNode(true); // Copy the template card
-        editCardContent(nextCard, title, imageURL); // Edit title and image
-        cardContainer.appendChild(nextCard); // Add new card to the container
-    }
+	button.innerText = 'close'
+	button.addEventListener('click', () => {
+		console.log('trying to close')
+		dialog.close()
+	})
+	for (country of countryDetail) {
+		if (country.Country == inputCountry.Country) {
+			image.setAttribute('src', inputCountry.URL)
+			name.innerHTML = country.Country
+			population.innerText = `Population ${country.Population}`
+			area.innerText = `Area in square miles ${country['Area (sq. mi.)']}`
+			gdp.innerText = `GDP ($ per capita) ${country['GDP ($ per capita)']}`
+			return true
+		}
+	}
+}
+function createCard(country, countryDetail) {
+	const card = document.createElement('div')
+	const image = document.createElement('img')
+	const name = document.createElement('h2')
+	const button = document.createElement('button')
+	const dialog = document.querySelector('dialog')
+	button.innerText = 'learn more'
+	button.addEventListener('click', () => {
+		console.log(country.Country, ' modal')
+		populateDialog(dialog, country, countryDetail)
+		dialog.showModal()
+	})
+	card.className = 'card'
+	image.setAttribute('src', country.URL)
+	name.innerHTML = country.Country
+	card.appendChild(image)
+	card.appendChild(name)
+	card.appendChild(button)
+	return card
 }
 
-function editCardContent(card, newTitle, newImageURL) {
-    card.style.display = "block";
+function showCards(filter) {
+	const cardContainer = document.getElementById('card-container')
+	cardContainer.innerHTML = ''
 
-    const cardHeader = card.querySelector("h2");
-    cardHeader.textContent = newTitle;
-
-    const cardImage = card.querySelector("img");
-    cardImage.src = newImageURL;
-    cardImage.alt = newTitle + " Poster";
-
-    // You can use console.log to help you debug!
-    // View the output by right clicking on your website,
-    // select "Inspect", then click on the "Console" tab
-    console.log("new card:", newTitle, "- html: ", card);
+	if (filter) {
+		for (const country of countryFlagSorted) {
+			if (country.Country.toLowerCase().includes(filter.toLowerCase())) {
+				cardContainer.appendChild(createCard(country, countryDetail))
+			}
+		}
+	} else {
+		for (const country of countryFlagSorted) {
+			cardContainer.appendChild(createCard(country, countryDetail))
+		}
+	}
 }
 
-// This calls the addCards() function when the page is first loaded
-document.addEventListener("DOMContentLoaded", showCards);
-
-function quoteAlert() {
-    console.log("Button Clicked!")
-    alert("I guess I can kiss heaven goodbye, because it got to be a sin to look this good!");
+async function loadData() {
+	countryFlag = await fetch('flags.json')
+		.then(response => response.json())
+		.then(json => {
+			return json
+		})
+	countryDetail = await fetch('details.json')
+		.then(response => response.json())
+		.then(json => {
+			return json
+		})
 }
 
-function removeLastCard() {
-    titles.pop(); // Remove last item in titles array
-    showCards(); // Call showCards again to refresh
+let countryFlagSorted
+let countryFlag
+let countryDetail
+let sortBy = 'name'
+loadData().then(() => {
+	countryFlagSorted = countryFlag
+	showCards()
+})
+
+const filter = document.querySelector('.filter')
+filter.addEventListener('input', () => {
+	const filterValue = filter.value
+	showCards(filterValue)
+})
+
+function sort(sortBy) {
+	let filteredCountry = []
+
+	switch (sortBy) {
+		case 'population':
+			filteredCountry = countryDetail.slice().sort((a, b) => {
+				return a.Population - b.Population
+			})
+			break
+		case 'area':
+			filteredCountry = countryDetail.slice().sort((a, b) => {
+				return a['Area (sq. mi.)'] - b['Area (sq. mi.)']
+			})
+			break
+	
+		//Data already sorted by Name
+		case 'name':
+			filteredCountry = countryDetail
+			break
+	}
+	let result = []
+	for (cf of filteredCountry) {
+		for (c of countryFlag) {
+			if (cf.Country == c.Country) {
+				result.push(c)
+			}
+		}
+	}
+
+	return result
+}
+
+function setSortVal(val) {
+	console.log('new sort val ', val)
+	sortBy = val
+	countryFlagSorted = sort(sortBy)
+	showCards()
 }
